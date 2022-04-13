f706d804954dbdb7de4557bd5813c94d6fdc48ca
Zadic <102991737+Zadic1@users.noreply.github.com>
2022-04-13 13:05:13 -0600

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,14 @@
 input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . # # # .
-        . . . # .
-        . . # # .
-        . . . # .
-        . # # # .
-        `)
-    basic.showLeds(`
-        . # # . .
-        . . . # .
-        . . # # .
-        . # . . .
-        . . # # .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        . . # . .
-        `)
+    strip.clear()
+    range3.showColor(neopixel.colors(NeoPixelColors.Red))
+    basic.pause(1000)
+    strip.clear()
+    range2.showColor(neopixel.colors(NeoPixelColors.Yellow))
+    basic.pause(1000)
+    strip.clear()
+    range.showColor(neopixel.colors(NeoPixelColors.Green))
+    basic.pause(1000)
+    strip.clear()
     random_number = randint(5, 15)
     while (random_number > 0) {
         basic.clearScreen()
@@ -47,7 +36,12 @@ input.onButtonPressed(Button.A, function () {
     }
 })
 let random_number = 0
-basic.showString("hey man")
-basic.forever(function () {
-	
-})
+let range3: neopixel.Strip = null
+let range2: neopixel.Strip = null
+let range: neopixel.Strip = null
+let strip: neopixel.Strip = null
+strip = neopixel.create(DigitalPin.P0, 15, NeoPixelMode.RGB)
+range = strip.range(11, 14)
+range2 = strip.range(6, 11)
+range3 = strip.range(0, 6)
+strip.showColor(neopixel.rgb(randint(0, 64), randint(0, 64), randint(0, 64)))
